e3c9caa5d56cb37c2739e5ef8b251b4e2deb7ca3
Yung Dai <yungchidai@gmail.com>
2014-11-05 13:19:59 -0500

    7. Moving @pictures in pictures_controller.rb into database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -3,46 +3,16 @@ class PicturesController < ApplicationController
   # this index method inside the controller generates an index page for the website.  The index.html.erb file is how
   # this method will be shown.
   def index
-
-    # inside the @pictures array are three pictures with the keys :title, :artist, :url
-    @pictures = [
-        {
-            :title  => "The old church on the coast of White sea",
-            :artist => "Sergey Ershov",
-            :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
-        },
-        {
-            :title  => "Sea Power",
-            :artist => "Stephen Scullion",
-            :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
-        },
-        {
-            :title  => "Into the Poppies",
-            :artist => "John Wilhelm",
-            :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
-        }
-    ]
+    # this instruction tells the the index will now get the pictures from the picture.rb program that has a Object
+    # Pictures class inside it.
+    @pictures = Pictures.all
   end
 
   # this show method stores the instructions of how the show.html.erb file will be displayed to the browser.
   def show
-    @pictures = [
-        {
-            :title  => "The old church on the coast of White sea",
-            :artist => "Sergey Ershov",
-            :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
-        },
-        {
-            :title  => "Sea Power",
-            :artist => "Stephen Scullion",
-            :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
-        },
-        {
-            :title  => "Into the Poppies",
-            :artist => "John Wilhelm",
-            :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
-        }
-    ]
+    # this instruction will find the picture based on the params :id (http://website/pictures/;id <--
+    # (the /pictures/:id/ is the params[:id])
+    @pictures = Pictures.find(params[:id])
     # this next line allows you to take the pull the picture :id from the address bar
     # example: http://website/pictures/:id and makes sure that the :id is an integer so really it's the following
     # when the line has been parsed:.  @pictures = @pictures[:id = 1]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Earlier we made three pictures in the controller. This is absolutely against good practices—the data should always
+# be going into the models. Let's fix this right now by editing the a file called seeds.rb:
+
+Picture.create!(
+    :title  => "The old church on the coast of White sea",
+    :artist => "Sergey Ershov",
+    :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
+)
+
+Picture.create!(
+    :title  => "Sea Power",
+    :artist => "Stephen Scullion",
+    :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+)
+
+Picture.create!(
+    :title  => "Into the Poppies",
+    :artist => "John Wilhelm",
+    :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
+)
+
