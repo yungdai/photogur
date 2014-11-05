e2c010384012b64c7589091e0be15939f9616303
Yung Dai <yungchidai@gmail.com>
2014-11-05 12:36:00 -0500

    Getting http://localhost:3000/pictures to work

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,7 @@
+class PicturesController < ApplicationController
+
+  # this index method inside the controller generates an index page for the website
+  def index
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # This adds a /pictures route (http://website/pictures) and tells it to go to the index.html.erb file
+  get 'pictures' => 'pictures#index'
+
+  # Ignore the comments below for now
+  # They are just documentation
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
